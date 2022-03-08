af4ed7e97af968e6aac847f2c2804300ff49642b
tsuyoshi.nakama <mohi.cocacola@gmail.com>
2022-03-09 01:13:17 +0900

    set up mock service worker.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
+
+import { worker } from './mocks/browser';
+
+worker.start();
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
